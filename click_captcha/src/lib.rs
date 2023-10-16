62b371d8160de5f12874f8eb3cb736812bda1110
3Ti <3Ti.Site@gmail.com>
2023-10-16 10:58:54 +0800

    ♨

--- click_captcha/src/lib.rs
+++ click_captcha/src/lib.rs
@@ -1,11 +1,11 @@
-mod flag;
-mod flag_li;
-mod pattern;
-mod random_pos;
+pub mod flag;
+pub mod flag_li;
+pub mod pattern;
+pub mod random_pos;
 pub mod svg;
-use svg2webp::svg2webp;
+pub use svg2webp::svg2webp;
 
-use crate::flag_li::{Flag, N};
+pub use crate::flag_li::{Flag, N};
 
 pub fn gen(width: u32, height: u32) -> Result<(Box<[u8]>, [Flag; N]), svg2webp::Error> {
   let (xml, flag_li) = svg::gen(width, height);
