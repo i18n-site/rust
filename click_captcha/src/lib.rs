07dad429b69b1b701f5e7f976b061deeb03c47ac
3Ti <3Ti.Site@gmail.com>
2023-10-17 11:11:23 +0800

    ♨

--- click_captcha/src/lib.rs
+++ click_captcha/src/lib.rs
@@ -17,16 +17,22 @@ fn distance(x1: f32, y1: f32, x2: f32, y2: f32) -> f32 {
   ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt()
 }
 
-pub fn verify(flag_x_y_size: &[u32], click_x_y: &[u32]) -> bool {
+pub fn verify(flag_x_y_size: &[u64], click_x_y: &[u64], scale: u64) -> bool {
   if (click_x_y.len() * 3) != (2 * flag_x_y_size.len()) {
     return false;
   }
 
   for (pos, xys) in flag_x_y_size.chunks(3).enumerate() {
     let pos = pos * 2;
-    let cx = click_x_y[pos] as f32;
-    let cy = click_x_y[pos + 1] as f32;
+    let cx = (click_x_y[pos] * scale) as f32;
+    let cy = (click_x_y[pos + 1] * scale) as f32;
     let size = (xys[2] as f32) / 2.0;
+    dbg!(
+      xys,
+      cx,
+      cy,
+      distance(xys[0] as f32 + size, xys[1] as f32 + size, cx, cy)
+    );
     if distance(xys[0] as f32 + size, xys[1] as f32 + size, cx, cy) > size {
       return false;
     }
