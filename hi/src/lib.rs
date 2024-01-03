a38a72edb3349a89fe14bae03552fb6a3a9059d7
3Ti <3Ti.Site@gmail.com>
2024-01-03 18:59:00 +0800

    ♨

--- hi/src/lib.rs
+++ hi/src/lib.rs
@@ -2,10 +2,13 @@ use aok::Result;
 
 pub async fn send(
   from_name: impl AsRef<str>,
+  to: impl AsRef<str>,
   title: impl AsRef<str>,
   txt: impl AsRef<str>,
   url: impl AsRef<str>,
 ) -> Result<()> {
+  let from_name = from_name.as_ref();
+  let to = to.as_ref();
   let title = title.as_ref();
   let txt = txt.as_ref().to_owned();
   let url = url.as_ref();
@@ -13,9 +16,10 @@ pub async fn send(
   let result_li = tokio::join!(
     xsmtp::send(
       from_name,
+      to,
       title,
       if url.is_empty() {
-        url.to_owned() + "\n\n" + &txt;
+        url.to_owned() + "\n\n" + &txt
       } else {
         txt
       },
@@ -31,4 +35,5 @@ pub async fn send(
       url
     )
   );
+  Ok(())
 }
