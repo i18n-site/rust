f0301cd6513b7fe8e56cd2f1ba03b48bd3fd5834
3Ti <3Ti.Site@gmail.com>
2024-01-03 21:50:11 +0800

    ♨

--- ireq/src/lib.rs
+++ ireq/src/lib.rs
@@ -1,4 +1,6 @@
-use reqwest::Client;
+use std::time::Duration;
+
+use reqwest::{Body, Client, IntoUrl, Response, Version};
 
 #[static_init::dynamic]
 pub static REQ: Client = Client::builder()
@@ -7,4 +9,18 @@ pub static REQ: Client = Client::builder()
   .build()
   .unwrap();
 
-pub async fn ireq() {}
+pub async fn get(url: impl IntoUrl) -> reqwest::Result<Response> {
+  REQ.get(url).version(Version::HTTP_3).send().await
+}
+
+macro_rules! method {
+  ($($method: ident),*) => {
+    $(
+    pub async fn $method(url: impl IntoUrl, body:impl Into<Body>) -> reqwest::Result<Response> {
+      REQ.$method(url).body(body).version(Version::HTTP_3).send().await
+    }
+    )*
+  };
+}
+
+method!(post, delete, patch, put);
