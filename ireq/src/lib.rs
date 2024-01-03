5190d45446fde38384f90e5adec9f2b2a846cdd2
3Ti <3Ti.Site@gmail.com>
2024-01-03 22:29:24 +0800

    ♨

--- ireq/src/lib.rs
+++ ireq/src/lib.rs
@@ -6,18 +6,21 @@ use reqwest::{Body, Client, IntoUrl, Response, Version};
 pub static REQ: Client = Client::builder()
   .timeout(Duration::from_secs(60))
   .http3_prior_knowledge()
+  .danger_accept_invalid_certs(true)
   .build()
   .unwrap();
 
 pub async fn get(url: impl IntoUrl) -> reqwest::Result<Response> {
   REQ.get(url).version(Version::HTTP_3).send().await
 }
-
 macro_rules! method {
   ($($method: ident),*) => {
     $(
     pub async fn $method(url: impl IntoUrl, body:impl Into<Body>) -> reqwest::Result<Response> {
-      REQ.$method(url).body(body).version(Version::HTTP_3).send().await
+      REQ.$method(url).body(body)
+        .version(Version::HTTP_3)
+        .send()
+        .await
     }
     )*
   };
