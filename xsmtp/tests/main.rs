db864532652ff6ee00f12964499f69735762137e
3Ti <3Ti.Site@gmail.com>
2024-01-03 17:31:18 +0800

    ♨

--- xsmtp/tests/main.rs
+++ xsmtp/tests/main.rs
@@ -1,11 +1,13 @@
 #[tokio::test]
 async fn test() -> anyhow::Result<()> {
-  xsmtp::async_send(
-    "测试xsmtp",
-    "i18n.site@foxmail.com",
-    "测试邮件",
-    "正文\n测试",
-    "",
+  Ok(
+    xsmtp::async_send(
+      "测试xsmtp",
+      "i18n.site@foxmail.com",
+      "测试邮件",
+      "正文\n测试",
+      "",
+    )
+    .await?,
   )
-  .await?;
 }
