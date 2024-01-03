c4592fb437b869688f3c2dae97761770ede3460a
3Ti <3Ti.Site@gmail.com>
2024-01-03 23:33:20 +0800

    ♨

--- alive/src/lib.rs
+++ alive/src/lib.rs
@@ -16,7 +16,7 @@ macro_rules! dberr {
     let err_type = stringify!($type);
     let msg = format!("DB ERROR {} : {}",err_type,err);
     tracing::warn!(msg);
-    xerr::log(hi::send(err_type,err.clone(),"https://atomgit.com/3ti/rust/blob/main/alive/src/lib.rs#L13").await);
+    hi::send(err_type,err.clone(),"https://atomgit.com/3ti/rust/blob/main/alive/src/lib.rs#L13").await;
   }};
 }
 
