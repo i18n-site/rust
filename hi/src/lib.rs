47b69e606567fb21ef04b7d70b8afbaa8e616a78
3Ti <3Ti.Site@gmail.com>
2024-01-03 23:09:43 +0800

    ♨

--- hi/src/lib.rs
+++ hi/src/lib.rs
@@ -2,24 +2,23 @@ pub mod lark;
 
 use aok::{Result, OK};
 
-genv::s!(TO_MAIL);
+genv::s!(TO_MAIL, NAME);
 
 pub async fn send(
-  from_name: impl AsRef<str>,
   title: impl AsRef<str>,
   txt: impl AsRef<str>,
   url: impl AsRef<str>,
 ) -> Result<()> {
-  let from_name = from_name.as_ref();
+  let name = NAME.as_ref();
   let title = title.as_ref();
   let txt = txt.as_ref();
   let url = url.as_ref();
 
-  let name_title = format!("{} · {}", from_name, title);
+  let name_title = format!("{} · {}", name, title);
 
   let result_li = tokio::join!(
     xsmtp::send(
-      from_name,
+      name,
       TO_MAIL.as_ref(),
       title,
       if url.is_empty() {
