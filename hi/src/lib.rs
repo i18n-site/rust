4007634c56ba715886ebe25af5ca4f9a3ff359a2
3Ti <3Ti.Site@gmail.com>
2024-01-03 19:02:55 +0800

    ♨

--- hi/src/lib.rs
+++ hi/src/lib.rs
@@ -1,16 +1,16 @@
-use aok::Result;
+use aok::{Result, OK};
 
 pub async fn send(
   from_name: impl AsRef<str>,
-  to: impl AsRef<str>,
+  to: impl Into<String>,
   title: impl AsRef<str>,
   txt: impl AsRef<str>,
   url: impl AsRef<str>,
 ) -> Result<()> {
   let from_name = from_name.as_ref();
-  let to = to.as_ref();
+  let to = to.into();
   let title = title.as_ref();
-  let txt = txt.as_ref().to_owned();
+  let txt = txt.as_ref();
   let url = url.as_ref();
 
   let result_li = tokio::join!(
@@ -19,9 +19,9 @@ pub async fn send(
       to,
       title,
       if url.is_empty() {
-        url.to_owned() + "\n\n" + &txt
+        url.to_owned() + "\n\n" + txt
       } else {
-        txt
+        txt.to_owned()
       },
       "",
     ),
@@ -35,5 +35,8 @@ pub async fn send(
       url
     )
   );
-  Ok(())
+  for i in result_li {
+    dbg!(i);
+  }
+  OK
 }
