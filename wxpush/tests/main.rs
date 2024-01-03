1c1ccc65d2ed78dd2e12dc565e39248125229d0b
3Ti <3Ti.Site@gmail.com>
2024-01-03 17:57:24 +0800

    ♨

--- wxpush/tests/main.rs
+++ wxpush/tests/main.rs
@@ -2,16 +2,16 @@
 async fn test() -> anyhow::Result<()> {
   let mut title = "测试推送".to_owned();
   while title.len() < 4000 {
-    title.push(title);
+    title.push_str(&title);
   }
   let mut body = "正文\n测试\n".to_owned();
   while body.len() < 999999 {
-    body.push(body);
+    body.push_str(&body);
   }
 
   let mut url = "https://atomgit.com/3ti".to_owned();
   while url.len() < 4000 {
-    url.push("123456790");
+    url.push_str("123456790");
   }
   Ok(wxpush::send(&url, &title, &body).await?)
 }
