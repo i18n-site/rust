ac6913eda297fe8ab7b00ebc849968fc94ffc3a8
3Ti <3Ti.Site@gmail.com>
2024-01-04 21:04:00 +0800

    ♨

--- aliver/src/main.rs
+++ aliver/src/main.rs
@@ -2,32 +2,34 @@ use std::net::SocketAddr;
 
 use axum::{body::Bytes, http::StatusCode, routing::post, Router};
 
-genv::def!(PORT:u16 | 8080);
+genv::def!(PORT:u16 | 5123);
 
 async fn index(json: Bytes) -> aerr::msg!() {
-  let subject;
-  let txt;
-  let status;
-  match sonic_rs::from_slice::<mail::Root>(&json) {
-    Err(e) => {
-      txt = String::from_utf8_lossy(&json);
-      let e = e.to_string();
-      subject = format!("mailhook json parse error : {}", &e);
-      tracing::error!("{}\n{}", e, &txt);
-      status = StatusCode::BAD_REQUEST;
-    }
-    Ok(root) => {
-      status = StatusCode::OK;
-      let payload = root.payload;
-      subject = payload.subject;
-      txt = payload.txt.into();
-    }
-  }
-  if status == StatusCode::OK {
-    Ok(())
-  } else {
-    aerr::err(status, ())
-  }
+  // let subject;
+  // let txt;
+  // let status;
+  // match sonic_rs::from_slice::<mail::Root>(&json) {
+  //   Err(e) => {
+  //     txt = String::from_utf8_lossy(&json);
+  //     let e = e.to_string();
+  //     subject = format!("mailhook json parse error : {}", &e);
+  //     tracing::error!("{}\n{}", e, &txt);
+  //     status = StatusCode::BAD_REQUEST;
+  //   }
+  //   Ok(root) => {
+  //     status = StatusCode::OK;
+  //     let payload = root.payload;
+  //     subject = payload.subject;
+  //     txt = payload.txt.into();
+  //   }
+  // }
+  // if status == StatusCode::OK {
+  //   Ok(())
+  // } else {
+  //   aerr::err(status, ())
+  // }
+
+  "123"
 }
 
 #[tokio::main]
