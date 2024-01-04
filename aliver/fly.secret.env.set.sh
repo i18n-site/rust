a5df8234e878ec37deaacc9b93d2c4edc6274405
3Ti <3Ti.Site@gmail.com>
2024-01-05 00:12:14 +0800

    ♨

--- aliver/fly.secret.env.set.sh
+++ aliver/fly.secret.env.set.sh
@@ -5,11 +5,12 @@ cd $DIR
 set -e
 
 setenv() {
+  all=""
   for i in "$@"; do
     eval "v=\"\$$i\""
-    echo "$i=$v"
-    direnv exec . flyctl secrets set $i="$v"
+    all="$all $i=$v"
   done
+  direnv exec . flyctl secrets set $all
 }
 
 setenv HEALTHCHECK LARK_BOT MYSQL_COMPRESS MYSQL_CONN_LIMIT MYSQL_DB MYSQL_HOST MYSQL_PORT MYSQL_PWD MYSQL_SSL MYSQL_USER NAME RUST_BACKTRACE RUST_LOG SMTP_FROM SMTP_HOST SMTP_PASSWORD SMTP_USER TO_MAIL WXPUSH_ID WXPUSH_TOKEN
