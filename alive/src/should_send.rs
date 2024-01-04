64d4438fffd1d614e8cae4bf5a166a48f9d3cd86
3Ti <3Ti.Site@gmail.com>
2024-01-04 14:02:22 +0800

    ♨

--- alive/src/should_send.rs
+++ alive/src/should_send.rs
@@ -4,13 +4,13 @@ pub fn is_power_of_two(n: u32) -> bool {
 
 pub fn should_send(err_count: u32, warn_err: u8) -> bool {
   let warn_err = warn_err as _;
-  if err_count > warn_err {
+  if err_count >= warn_err {
     let diff = err_count - warn_err;
     if diff > 1440 {
       diff % 1439 == 0
     } else {
       // 1=4(W) 2=5 3=6 4=7 5=8(W)
-      is_power_of_two(diff + 3)
+      is_power_of_two(diff + 4)
     }
   } else {
     false
