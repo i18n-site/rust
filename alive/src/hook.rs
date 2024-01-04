1c6f9127dc5de6d561abfd1fdb66d3e7b2f6c99e
3Ti <3Ti.Site@gmail.com>
2024-01-04 12:15:26 +0800

    ♨

--- alive/src/hook.rs
+++ alive/src/hook.rs
@@ -4,17 +4,13 @@ pub async fn smtp() -> Result<()> {
   OK
 }
 
-async fn log(result: Result<()>) {
-  xerr::log!(async move { result }.await);
-}
-
 macro_rules! hook {
   ($($fn:ident),*) => {
     pub async fn hook(name: &str) -> bool {
       match name {
         $(
           stringify!($fn) => {
-            log(crate::watch($fn().await).await);
+            crate::watch($fn().await).await;
             true
           }
         ),*
