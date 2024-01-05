895b0491bf156916a74ee7f6f4680daa1a1cbe86
3Ti <3Ti.Site@gmail.com>
2024-01-05 23:23:54 +0800

    ♨

--- idns/src/lib.rs
+++ idns/src/lib.rs
@@ -1,1 +1,13 @@
-pub fn idns() {}
+use hickory_resolver::{
+  config::{ResolverConfig, ResolverOpts},
+  TokioAsyncResolver,
+};
+
+#[static_init::dynamic]
+pub static RESOLVER: [TokioAsyncResolver; 2] = [
+  TokioAsyncResolver::tokio(ResolverConfig::cloudflare(), ResolverOpts::default()),
+  TokioAsyncResolver::tokio(ResolverConfig::google(), ResolverOpts::default()),
+];
+
+//     match RESOLVER.lookup(host, RecordType::$rec_type).await {
+// pub fn lookup() {}
