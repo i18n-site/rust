27ff3a125d875fa7ddcba601c262fbf9be6cb28d
3Ti <3Ti.Site@gmail.com>
2024-01-05 11:23:18 +0800

    .

--- mysql_macro/src/lib.rs
+++ mysql_macro/src/lib.rs
@@ -1,8 +1,6 @@
 #![feature(macro_metavar_expr)]
 
-mod id_v;
 use genv::def;
-pub use id_v::{id_v, id_v_str};
 pub use mysql_async::{
   self,
   prelude::{Query, Queryable, WithParams},
@@ -96,4 +94,39 @@ macro_rules! q1 {
     ($sql:expr $(,$arg:expr)* $(,)?) => {
         $crate::q01!($sql $(,$arg)*).unwrap()
     };
+}
+
+/*
+
+因为这个错误
+macro-expanded `macro_export` macros from the current crate cannot be referred to by absolute paths
+
+下面代码必须放到 lib.rs
+*/
+
+use std::collections::HashMap;
+
+use xstr::Join;
+
+pub async fn id_v<S: Send + 'static>(
+  table: &str,
+  id_set: impl IntoIterator<Item = u64>,
+) -> Result<HashMap<u64, S>>
+where
+  (u64, S): mysql_async::prelude::FromRow,
+{
+  let id_set = id_set.join(",");
+  if id_set.is_empty() {
+    return Ok(Default::default());
+  }
+
+  let li: Vec<(u64, S)> = q!(format!("SELECT id,v FROM {table} WHERE id IN ({})", id_set));
+  Ok(HashMap::from_iter(li.into_iter()))
+}
+
+pub async fn id_v_str(
+  table: &str,
+  id_set: impl IntoIterator<Item = u64>,
+) -> Result<HashMap<u64, String>> {
+  id_v(table, id_set).await
 }
