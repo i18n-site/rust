84070942c7b2e9e66403c13aeaec722a00ccc328
3Ti <3Ti.Site@gmail.com>
2024-01-06 02:56:58 +0800

    ♨

--- alive/src/status.rs
+++ alive/src/status.rs
@@ -9,7 +9,7 @@ use len_trait::len::Len;
 use mysql_macro as m;
 
 use crate::{
-  api::{Check, IdName, State, StateLi},
+  api::{Check, HostStateLi, IdName, KindStateLi, State, StateLi},
   cron,
   db::Status,
 };
@@ -20,6 +20,39 @@ pub static HOST: DashMap<u64, String> = DashMap::new();
 #[static_init::dynamic]
 pub static KIND: DashMap<u64, String> = DashMap::new();
 
+pub fn kind_state_li(li: impl Len + IntoIterator<Item = Status>) -> Vec<KindStateLi> {
+  let mut m: HashMap<u64, HashMap<u64, Vec<State>>> = HashMap::new();
+  for i in li {
+    let state = State {
+      dns_type: i.dns_type as _,
+      err: i.err,
+      ts: i.ts,
+    };
+    if let Some(v) = m.get_mut(&i.kind_id) {
+      if let Some(h) = v.get_mut(&i.host_id) {
+        h.push(state);
+      } else {
+        v.insert(i.host_id, vec![state]);
+      }
+    } else {
+      m.insert(i.kind_id, HashMap::from([(i.host_id, vec![state])]));
+    };
+  }
+  m.into_iter()
+    .map(|i| KindStateLi {
+      kind_id: i.0,
+      li: i
+        .1
+        .into_iter()
+        .map(|i| HostStateLi {
+          host_id: i.0,
+          li: i.1,
+        })
+        .collect(),
+    })
+    .collect()
+}
+
 pub fn to_id_name(li: impl Len + IntoIterator<Item = (u64, String)>) -> Vec<IdName> {
   let mut id_name = Vec::with_capacity(li.len());
   for i in li {
@@ -29,8 +62,7 @@ pub fn to_id_name(li: impl Len + IntoIterator<Item = (u64, String)>) -> Vec<IdNa
 }
 
 pub async fn status() -> Result<StateLi> {
-  let li: Vec<Status> =
-    m::q!("SELECT kind_id,host_id,dns_type,err,ts FROM watch ORDER BY err DESC,kind_id,host_id");
+  let li: Vec<Status> = m::q!("SELECT kind_id,host_id,dns_type,err,ts FROM watch");
 
   let mut host_id_li = HashSet::new();
   let mut kind_id_li = HashSet::new();
@@ -46,7 +78,7 @@ pub async fn status() -> Result<StateLi> {
 
   if !host_id_li.is_empty() {
     for i in m::id_v_str("host", host_id_li).await? {
-      HOST.insert(i.0, i.1);
+      HOST.insert(i.0, i.1[..i.1.rfind('-').unwrap_or(i.1.len())].to_owned());
     }
   }
   if !kind_id_li.is_empty() {
@@ -58,28 +90,28 @@ pub async fn status() -> Result<StateLi> {
   let mut kind = HashMap::new();
   let mut host = HashMap::new();
 
-  for i in &li {
+  let mut ok = Vec::with_capacity(li.len());
+  let mut err = Vec::new();
+
+  for i in li {
     if let Some(v) = KIND.get(&i.kind_id).map(|i| i.clone()) {
       kind.insert(i.kind_id, v);
     }
     if let Some(v) = HOST.get(&i.host_id).map(|i| i.clone()) {
       host.insert(i.host_id, v);
     }
+    if i.err == 0 {
+      ok.push(i);
+    } else {
+      err.push(i);
+    }
   }
 
   Ok(StateLi {
     kind: to_id_name(kind),
     host: to_id_name(host),
-    li: li
-      .into_iter()
-      .map(|i| State {
-        kind_id: i.kind_id,
-        host_id: i.host_id,
-        dns_type: i.dns_type as _,
-        err: i.err,
-        ts: i.ts,
-      })
-      .collect(),
+    ok: kind_state_li(ok),
+    err: kind_state_li(err),
     check: Some(Check {
       last: cron::TS.load(Relaxed),
       count: cron::COUNT.load(Relaxed),
