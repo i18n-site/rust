06e125e396ac484845d41c19799ee8c443a0ed4f
3Ti <3Ti.Site@gmail.com>
2024-01-05 18:21:53 +0800

    ♨

--- aliver/src/main.rs
+++ aliver/src/main.rs
@@ -6,7 +6,7 @@ use aok::{Result, OK};
 use axum::{
   body::Body,
   extract::Request,
-  http::{self, HeaderValue},
+  http::{header, HeaderValue},
   middleware,
   middleware::Next,
   response::IntoResponse,
@@ -19,14 +19,34 @@ use tower::ServiceBuilder;
 genv::def!(PORT:u16 | 5123);
 genv::s!(HEALTHCHECK);
 
-pub static TEXT_JSON: &str = "text/json";
-
 async fn header(req: Request<Body>, next: Next) -> impl IntoResponse {
+  let origin = req
+    .headers()
+    .get(header::ORIGIN)
+    .map(|i| i.to_str().map(|s| s.to_owned()));
   let mut res = next.run(req).await;
-  res.headers_mut().insert(
-    http::header::CONTENT_TYPE,
-    HeaderValue::from_static(TEXT_JSON),
+
+  let headers = res.headers_mut();
+  headers.insert(
+    header::ACCESS_CONTROL_ALLOW_CREDENTIALS,
+    HeaderValue::from_static("true"),
   );
+  if let Some(Ok(origin)) = origin {
+    headers.insert(
+      header::ACCESS_CONTROL_ALLOW_ORIGIN,
+      HeaderValue::from_str(&origin).unwrap(),
+    );
+  }
+  headers.insert(
+    header::ACCESS_CONTROL_ALLOW_METHODS,
+    HeaderValue::from_static("*"),
+  );
+  // headers.insert(header::ACCESS_CONTROL_ALLOW_HEADERS, HeaderValue::from_static("content-type"));
+
+  // res
+  //   .headers_mut()
+  //   .insert(AccessControlAllowHeaders::from("content-type"));
+
   res
 }
 
