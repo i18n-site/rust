8c71ef00ff78c46b79b244d5c9c6d38e5f41f0bb
3Ti <3Ti.Site@gmail.com>
2024-01-05 23:36:43 +0800

    ♨

--- idns/src/lib.rs
+++ idns/src/lib.rs
@@ -1,13 +1,23 @@
+use core::net::Ipv4Addr;
+use std::net::IpAddr;
+
 use hickory_resolver::{
-  config::{ResolverConfig, ResolverOpts},
+  config::{NameServerConfigGroup, ResolverConfig, ResolverOpts},
   TokioAsyncResolver,
 };
 
-#[static_init::dynamic]
-pub static RESOLVER: [TokioAsyncResolver; 2] = [
-  TokioAsyncResolver::tokio(ResolverConfig::cloudflare(), ResolverOpts::default()),
-  TokioAsyncResolver::tokio(ResolverConfig::google(), ResolverOpts::default()),
-];
+// #[static_init::dynamic]
+// pub static RESOLVER: [TokioAsyncResolver; 4] = [
+// TokioAsyncResolver::tokio(ResolverConfig::cloudflare(), ResolverOpts::default()),
+// TokioAsyncResolver::tokio(ResolverConfig::google_tls(), ResolverOpts::default()),
+// TokioAsyncResolver::tokio(ResolverConfig::google(), ResolverOpts::default()),
+// TokioAsyncResolver::tokio(ResolverConfig::cloudflare_tls(), ResolverOpts::default()),
+// ];
 
 //     match RESOLVER.lookup(host, RecordType::$rec_type).await {
 // pub fn lookup() {}
+#[static_init::dynamic]
+pub static RESOLVER: NameServerConfigGroup =
+  NameServerConfigGroup::from_ips_clear(&[IpAddr::V4(Ipv4Addr::new(223, 5, 5, 5))], 53, true);
+
+// from_ips_clear
