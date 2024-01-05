4805e4d190195ae9ca410c25640088c830860ba2
3Ti <3Ti.Site@gmail.com>
2024-01-05 19:55:44 +0800

    ♨

--- aliver/fly.dist.sh
+++ aliver/fly.dist.sh
@@ -4,8 +4,7 @@ DIR=$(realpath ${0%/*})
 cd $DIR
 set -ex
 
-cd ~/.fly
-cp -f iuser.config.yml config.yml
+./fly.config.sh
 if ! command -v fly &>/dev/null; then
   curl -L https://fly.io/install.sh | sh
 fi
