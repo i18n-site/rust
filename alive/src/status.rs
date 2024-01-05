0fcefbba524d81cd89139844274977c6604b520d
3Ti <3Ti.Site@gmail.com>
2024-01-05 23:12:43 +0800

    ♨

--- alive/src/status.rs
+++ alive/src/status.rs
@@ -81,9 +81,9 @@ pub async fn status() -> Result<StateLi> {
       })
       .collect(),
     check: Some(Check {
-      last: 0,  //cron::TS.load(Relaxed),
-      count: 0, //cron::COUNT.load(Relaxed),
-      cost: 0,  //cron::DURATION.load(Relaxed),
+      last: cron::TS.load(Relaxed),
+      count: cron::COUNT.load(Relaxed),
+      cost: cron::DURATION.load(Relaxed),
     }),
   })
 }
