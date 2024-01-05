38d5e9892e71e52d0e8b530651e69e4e745118ae
3Ti <3Ti.Site@gmail.com>
2024-01-05 15:32:41 +0800

    ♨

--- aliver/src/main.rs
+++ aliver/src/main.rs
@@ -2,26 +2,20 @@
 mod index;
 use index::index;
 mod ping;
-use std::{net::SocketAddr, time::Duration};
 
-use aok::Result;
+use aok::{Result, OK};
 use axum::{
   body::Body,
-  error_handling::HandleErrorLayer,
   extract::Request,
-  http::{self, HeaderValue, StatusCode},
+  http::{self, HeaderValue},
   middleware,
   middleware::Next,
   response::IntoResponse,
   routing::get,
-  BoxError, Router,
+  Router,
 };
 use ping::ping;
 use tower::ServiceBuilder;
-use tower_http::compression::{
-  predicate::{NotForContentType, Predicate, SizeAbove},
-  CompressionLayer,
-};
 
 genv::def!(PORT:u16 | 5123);
 genv::s!(HEALTHCHECK);
@@ -37,11 +31,9 @@ async fn header(req: Request<Body>, next: Next) -> impl IntoResponse {
   res
 }
 
-const TIMEOUT: u64 = 600;
-
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
-  tokio::spawn(async {
+  tokio::task::spawn_blocking(async || {
     let healthcheck = HEALTHCHECK.as_str();
     alive::cron::run(async move || {
       xerr::log!(ireq::get(healthcheck).await);
@@ -49,36 +41,12 @@ async fn main() -> Result<()> {
     .await;
   });
 
-  let middleware = ServiceBuilder::new()
-    .layer(HandleErrorLayer::new(|error: BoxError| async move {
-      if error.is::<tower::timeout::error::Elapsed>() {
-        Ok((StatusCode::REQUEST_TIMEOUT, "timeout"))
-      } else {
-        Err((
-          StatusCode::INTERNAL_SERVER_ERROR,
-          format!("Internal Error: {}", error),
-        ))
-      }
-    }))
-    .timeout(Duration::from_secs(TIMEOUT).into())
-    .layer(ServiceBuilder::new());
-
-  loginit::init();
-  let predicate = SizeAbove::new(256)
-    .and(NotForContentType::GRPC)
-    .and(NotForContentType::IMAGES);
-
   let app = Router::new()
     .route("/", get(re::FnAny(index)))
     .route("/ping", get(re::FnAny(ping)))
-    .layer(middleware)
-    .layer(CompressionLayer::new().compress_when(predicate))
     .layer(ServiceBuilder::new().layer(middleware::from_fn(header)));
-  let addr = SocketAddr::from(([0, 0, 0, 0], PORT()));
 
-  tracing::info!("http://{}", addr);
+  t3::srv(app).await;
 
-  axum::serve(tokio::net::TcpListener::bind(addr).await?, app).await?;
-
-  Ok(())
+  OK
 }
