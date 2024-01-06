79e0a928b460482c829e99fe2426f4b3741dbe60
3Ti <3Ti.Site@gmail.com>
2024-01-06 19:43:48 +0800

    ♨

--- idns/src/lib.rs
+++ idns/src/lib.rs
@@ -98,7 +98,7 @@ pub async fn AAAA<N: IntoName>(name: N) -> Result<Vec<std::net::IpAddr>, Resolve
   ip_li!(name, AAAA, V6)
 }
 
-pub async fn resolve<N: IntoName>(name: N) -> Result<Vec<std::net::IpAddr>, ResolveError> {
+pub async fn ip<N: IntoName>(name: N) -> Result<Vec<std::net::IpAddr>, ResolveError> {
   let name = name.into_name()?;
   if let Ok(r) = AAAA(name.clone()).await {
     if !r.is_empty() {
--- idns/tests/main.rs
+++ idns/tests/main.rs
@@ -2,11 +2,8 @@ use aok::{Result, OK};
 
 #[tokio::test]
 async fn test() -> Result<()> {
-  use idns::{A, AAAA};
   let host = "mail.i18n.site";
-  let r = A(host).await?;
-  dbg!(&r);
-  // let r = AAAA(host).await?;
-  // dbg!(&r);
+  let r = idns::ip(host).await?;
+  dbg!(r);
   OK
 }
