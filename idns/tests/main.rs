e8cf72ef1f44537479cb05b462e8b412461c8999
3Ti <3Ti.Site@gmail.com>
2024-01-06 19:16:59 +0800

    ♨

--- idns/tests/main.rs
+++ idns/tests/main.rs
@@ -1,7 +1,7 @@
-use aok::{Result,OK}
+use aok::{Result, OK};
 
 #[tokio::test]
-async fn test()->Result<()> {
+async fn test() -> Result<()> {
   use idns::{A, AAAA};
   let host = "mail.i18n.site";
   let li = A(host).await?;
