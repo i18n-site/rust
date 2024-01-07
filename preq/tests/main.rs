13356f0b714b187feb13a784b96f1a6b275ba051
3Ti <3Ti.Site@gmail.com>
2024-01-07 14:17:32 +0800

    ♨

--- preq/tests/main.rs
+++ preq/tests/main.rs
@@ -1,5 +1,11 @@
 #[test]
-fn test() {}
+fn test() {
+  use preq::{IPV6_PROXY, IPV6_PROXY_PORT};
+  let proxy: String = IPV6_PROXY();
+  let port: String = IPV6_PROXY_PORT();
+  let url = format!("http://{}:{}", proxy, port);
+  dbg!(url);
+}
 
 /*
 #[cfg(feature = "macro")]
