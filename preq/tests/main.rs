278d55dd13170f81f791dee0f550cdadf576626f
3Ti <3Ti.Site@gmail.com>
2024-01-07 14:34:40 +0800

    ♨

--- preq/tests/main.rs
+++ preq/tests/main.rs
@@ -5,7 +5,7 @@ async fn test() -> aok::Result<()> {
   let port: String = IPV6_PROXY_PORT();
   let url = format!("http://{}:{}", proxy, port);
   let proxy = preq::proxy(url.as_str());
-  let r = preq::post(&proxy, "http://baidu.com", "").await?;
+  let r = preq::post(&proxy, "http://baidu.com", |req| req).await?;
   dbg!(proxy);
   dbg!(r.text().await?);
   aok::OK
