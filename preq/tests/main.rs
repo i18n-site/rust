034008110c972b2070912a275e2468aaaf5f5333
3Ti <3Ti.Site@gmail.com>
2024-01-07 14:19:32 +0800

    ♨

--- preq/tests/main.rs
+++ preq/tests/main.rs
@@ -1,10 +1,12 @@
-#[test]
-fn test() {
+#[tokio::test]
+async fn test() -> aok::Result<()> {
   use preq::{IPV6_PROXY, IPV6_PROXY_PORT};
   let proxy: String = IPV6_PROXY();
   let port: String = IPV6_PROXY_PORT();
   let url = format!("http://{}:{}", proxy, port);
-  dbg!(url);
+  let proxy = preq::proxy(url.as_str());
+  preq::post(proxy, "http://baidu.com", "").await;
+  dbg!(proxy);
 }
 
 /*
