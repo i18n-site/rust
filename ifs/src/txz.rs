d6800f48a0b647458fb5482ca6a5346f5eef12e5
i18n <i18n.site@gmail.com>
2024-02-05 22:06:49 +0800

    ♨

--- ifs/src/txz.rs
+++ ifs/src/txz.rs
@@ -1,14 +1,13 @@
 use std::io::BufReader;
 
 use aok::{Result, OK};
-use tokio::task::spawn_blocking;
 
 use crate::pipe::channel;
 
 pub fn d(path: impl AsRef<std::path::Path>, to: impl AsRef<std::path::Path>) -> Result<()> {
   let (mut w, r) = channel(512);
   let mut f = BufReader::new(std::fs::File::open(path)?);
-  spawn_blocking(move || xerr::log!(lzma_rs::xz_decompress(&mut f, &mut w)));
+  let _ = std::thread::spawn(move || xerr::log!(lzma_rs::xz_decompress(&mut f, &mut w)));
   tar::Archive::new(r).unpack(to)?;
   OK
 }
