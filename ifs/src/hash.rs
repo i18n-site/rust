4ae181a3263afa196fe0511ec34a2448a3bec662
i18n <i18n.site@gmail.com>
2024-02-06 14:11:35 +0800

    b3s/0.1.85

--- ifs/src/hash.rs
+++ ifs/src/hash.rs
@@ -1,16 +1,13 @@
 use std::path::Path;
 
 use blake3::Hasher;
-use tokio::{
-  fs::File,
-  io::{AsyncReadExt, BufReader},
-};
+use tokio::{fs::File, io::AsyncReadExt};
 
 pub async fn hash(path: impl AsRef<Path>) -> Result<[u8; 32], std::io::Error> {
   let mut hasher = Hasher::new();
-  let mut file = BufReader::new(File::open(path).await?);
+  let mut file = File::open(path).await?;
 
-  let mut buf = [0; 65536];
+  let mut buf = [0; 16384];
   loop {
     let n = file.read(&mut buf).await?;
     if n == 0 {
