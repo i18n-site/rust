9ab15c6b340912bfedc9d87c905a5c8541381abb
i18n <i18n.site@gmail.com>
2024-03-28 08:35:54 +0800

    ♨

--- i18n-site/src/upload.rs
+++ i18n-site/src/upload.rs
@@ -107,7 +107,7 @@ impl Upload for NoUpload {
         Default::default()
       };
       let site_lang = api::SiteLang {
-        nav_i18n_li: nav_li
+        i18n_li: nav_li
           .iter()
           .map(|nav| nav_i18n.get(nav).unwrap_or(&EMPTY).to_owned())
           .collect(),
