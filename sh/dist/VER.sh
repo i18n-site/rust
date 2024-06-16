ca46785d7f1bd108ea4af2b1ab218ca858d2996b
i18n <i18n.site@gmail.com>
2024-06-16 15:34:22 +0800

    .

--- sh/dist/VER.sh
+++ sh/dist/VER.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-[ -z "${VER}" ] && export VER=$(cargo metadata --format-version=1 --no-deps | jq '.packages[] | .name + " " + .version' -r | grep "$(echo $PROJECT | sed 's/\./-/g') " | awk '{print $2}') || true
+echo $(cargo metadata --format-version=1 --no-deps | jq '.packages[] | .name + " " + .version' -r | grep "$(echo $PROJECT | sed 's/\./-/g') " | awk '{print $2}') || true
