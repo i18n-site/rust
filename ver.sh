d9212c898f23682f2bbc9988395a3aec1fffae88
i18n <i18n.site@gmail.com>
2024-06-16 15:18:07 +0800

    .

--- ver.sh
+++ ver.sh
@@ -7,47 +7,53 @@ cd $ROOT
 
 set -ex
 
-git add . && git commit -m. || true
+dist() {
+  git add . && git commit -m. || true
 
-branch=$(git symbolic-ref --short -q HEAD || echo main)
-if [ "$branch" != "main" ]; then
-  git fetch origin $branch
-  git merge origin/$branch -m merge
-fi
+  branch=$(git symbolic-ref --short -q HEAD || echo main)
+  if [ "$branch" != "main" ]; then
+    git fetch origin $branch
+    git merge origin/$branch -m merge
+  fi
 
-$ROOT/cargo.dist.sh $PROJECT
-beginhash=$(git log --format=%H -1 main)
+  $ROOT/cargo.dist.sh $PROJECT
+  beginhash=$(git log --format=%H -1 main)
 
-. $DIST/VER.sh
-PV=$PROJECT/$VER
+  . $DIST/VER.sh
+  PV=$PROJECT/$VER
 
-if [ "$branch" != "main" ]; then
-  git fetch origin main
-  git merge origin/main -m merge
-  git merge main -m merge
-  git checkout main
-  git merge $branch -m merge
-fi
+  if [ "$branch" != "main" ]; then
+    git fetch origin main
+    git merge origin/main -m merge
+    git merge main -m merge
+    git checkout main
+    git merge $branch -m merge
+  fi
 
-cd $ROOT
-git reset --soft $beginhash || true
-git add .
-git commit -m $PV || true
-
-git tag $PV
-
-push() {
-  git push $1 main
-  git push $1 $PV
-}
-
-push origin
-push github
-
-if [ "$branch" != "main" ]; then
-  git checkout $branch
-  git reset --hard $(git log --format=%H -1 main)
+  cd $ROOT
+  git reset --soft $beginhash || true
   git add .
   git commit -m $PV || true
-  git push -f
-fi
+
+  git tag $PV
+
+  push() {
+    git push $1 main
+    git push $1 $PV
+  }
+
+  push origin
+  push github
+
+  if [ "$branch" != "main" ]; then
+    git checkout $branch
+    git reset --hard $(git log --format=%H -1 main)
+    git add .
+    git commit -m $PV || true
+    git push -f
+  fi
+}
+
+for arg in "$@"; do
+  dist $arg
+done
