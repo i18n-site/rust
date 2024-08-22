ff49ac0ee169e48406919b3ecd1ef42f07367dc3
i18n <i18n.site@gmail.com>
2024-08-22 10:51:34 +0800

    i18:0.1.265

--- i18/src/env.rs
+++ i18/src/env.rs
@@ -1,9 +1,33 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Env {
-  pub token: Option<String>,
+pub struct S3 {
+  pub endpoint: String,
+  pub region: Option<String>,
+  pub ak: String,
+  pub sk: String,
 }
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct Conf {
+  pub token: Option<String>,
+  pub s3: Option<HashMap<String, Vec<S3>>>,
+}
+
+#[static_init::dynamic]
+pub static CONF: Conf = {
+  let yml = ifs::confdir().join("i18n.site.yml");
+  if let Ok(r) = xerr::ok!(ifs::r(yml)) {
+    if !r.is_empty() {
+      if let Ok::<Conf, _>(c) = serde_yaml::from_slice(&r) {
+        return c;
+      }
+    }
+  }
+  Default::default()
+};
 
 pub fn token() -> Option<String> {
   if let Ok(token) = std::env::var("I18N_SITE_TOKEN") {
@@ -11,13 +35,5 @@ pub fn token() -> Option<String> {
       return Some(token);
     }
   }
-  let yml = ifs::confdir().join("i18n.site.yml");
-  if let Ok(r) = xerr::ok!(ifs::r(yml)) {
-    if !r.is_empty() {
-      if let Ok::<Env, _>(env) = serde_yaml::from_slice(&r) {
-        return env.token;
-      }
-    }
-  }
-  None
+  CONF.token.clone()
 }
