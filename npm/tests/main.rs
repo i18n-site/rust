effa07da4ab1387d72e748601d69286a351a56ed
i18n <i18n.site@gmail.com>
2024-10-25 14:15:30 +0800

    i18n-site:0.2.89

--- npm/tests/main.rs
+++ npm/tests/main.rs
@@ -11,16 +11,16 @@ extern "C" fn init() {
 
 #[tokio::test]
 async fn test() -> Result<()> {
-  let token = npm::token();
-  if token.is_empty() {
-    info!("npm token is empty");
-  } else {
-    let dir: PathBuf = env!("CARGO_MANIFEST_DIR").into();
-    let src = dir.join("tests").join("pkg");
-    let package_json = src.join("package.json");
-    info!("begin publish");
-    // xerr::log!(npm::publish(&token, src, &package_json).await);
-  }
-  info!("done");
+  // let token = npm::token();
+  // if token.is_empty() {
+  //   info!("npm token is empty");
+  // } else {
+  //   let dir: PathBuf = env!("CARGO_MANIFEST_DIR").into();
+  //   let src = dir.join("tests").join("pkg");
+  //   let package_json = src.join("package.json");
+  //   info!("begin publish");
+  //   // xerr::log!(npm::publish(&token, src, &package_json).await);
+  // }
+  // info!("done");
   OK
 }
