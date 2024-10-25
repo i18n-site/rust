13e043af2be418008758d09a5092a0e020becc74
i18n <i18n.site@gmail.com>
2024-10-25 21:42:01 +0800

    i18:0.1.278

--- sh/dist/target.sh
+++ sh/dist/target.sh
@@ -4,4 +4,9 @@ set -ex
 
 rustup target list --installed | grep -q $1 && exit 0
 rustup target add $1
-rustup update nightly
+
+if [[ -n $RUST_VER ]]; then
+  rustup toolchain install nightly-$RUST_VER
+else
+  rustup update nightly
+fi
