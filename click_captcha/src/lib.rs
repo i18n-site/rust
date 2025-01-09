19918fa50b617ce27165c298a844b3756377a43c
i18n <i18n.site@gmail.com>
2025-01-09 13:03:55 +0800

    click_captcha:0.1.27

--- click_captcha/src/lib.rs
+++ click_captcha/src/lib.rs
@@ -41,27 +41,7 @@ pub fn make<S: AsRef<str>>(
 }
 
 #[cfg(feature = "verify")]
-fn distance(x1: f32, y1: f32, x2: f32, y2: f32) -> f32 {
-  ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt()
-}
+mod verify;
 
 #[cfg(feature = "verify")]
-pub fn verify(pos_li: PosLi, click_x_y: &[u32]) -> bool {
-  if click_x_y.len() < 2 * pos_li.len() {
-    return false;
-  }
-
-  for (pos, xys) in pos_li.iter().enumerate() {
-    let pos = pos * 2;
-    let cx = (click_x_y[pos]) as f32;
-    let cy = (click_x_y[pos + 1]) as f32;
-
-    // 起点 + 半径 = 圆心
-    let size = (xys.size as f32) / 2.0;
-
-    if distance(xys.x as f32 + size, xys.y as f32 + size, cx, cy) > size {
-      return false;
-    }
-  }
-  true
-}
+pub use verify::verify;
