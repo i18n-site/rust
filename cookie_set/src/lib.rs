28115752d7bb338f560d50244ae1e805badf4593
i18n <i18n.site@gmail.com>
2025-03-04 22:06:54 +0800

    cookie_set:0.1.5

--- cookie_set/src/lib.rs
+++ cookie_set/src/lib.rs
@@ -1,33 +1,29 @@
-use http::HeaderMap;
+use http::HeaderValue;
 
-pub const MAX: u32 = 99999999;
+const MAX: u32 = 99999999;
 
-pub struct Cookie<'a> {
+pub struct Cookie {
   pub domain: String,
-  pub headers: &'a mut HeaderMap,
 }
 
-pub fn new<'a>(domain: impl Into<String>, headers: &'a mut HeaderMap) -> Cookie<'a> {
+pub fn new(domain: impl Into<String>) -> Cookie {
   Cookie {
     domain: domain.into(),
-    headers,
   }
 }
 
-impl Cookie<'_> {
-  pub fn set_max(&mut self, key: impl AsRef<str>, val: impl AsRef<str>) {
-    self.set(key, val, MAX);
-  }
-
-  pub fn set(&mut self, key: impl AsRef<str>, val: impl AsRef<str>, max_age: u32) {
+impl Cookie {
+  pub fn set(&self, key: impl AsRef<str>, val: impl AsRef<str>, max_age: u32) -> HeaderValue {
     let key = key.as_ref();
     let val = val.as_ref();
     let cookie = format!(
       "{key}={val};Max-Age={max_age};Domain={};Secure;HttpOnly;Path=/;Partitioned",
       self.domain
     );
-    if let Ok(cookie) = cookie.parse() {
-      self.headers.append("set-cookie", cookie);
-    }
+    cookie.parse().unwrap()
+  }
+
+  pub fn set_max(&self, key: impl AsRef<str>, val: impl AsRef<str>) -> HeaderValue {
+    self.set(key, val, MAX)
   }
 }
