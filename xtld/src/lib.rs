48f971b02d49599ea2c7aa632926614071c96699
i18n <i18n.site@gmail.com>
2025-03-05 15:09:34 +0800

    xtld:0.1.6

--- xtld/src/lib.rs
+++ xtld/src/lib.rs
@@ -1,40 +1,40 @@
 use xstr::is_ascii_digit;
 
 /// x.x.com -> x.com
-pub fn host_tld(domain: impl AsRef<str>) -> String {
-  let domain = domain.as_ref().as_bytes();
-  let mut domain = domain;
-  if let Some(d) = psl::domain(domain) {
+pub fn tld(domain: &str) -> &str {
+  let bin = domain.as_bytes();
+  let mut bin = bin;
+  if let Some(d) = psl::domain(bin) {
     let bytes = d.suffix().as_bytes();
     let len = bytes.len();
     if len > 0 && !is_ascii_digit(bytes) {
-      let mut n = domain.len() - len;
+      let mut n = bin.len() - len;
       n = n.saturating_sub(1);
       while n > 0 {
         let t = n - 1;
-        if domain[t] == b'.' {
+        if bin[t] == b'.' {
           break;
         }
         n = t;
       }
-      domain = &domain[n..]
+      bin = &bin[n..]
     }
   }
-  unsafe { String::from_utf8_unchecked(domain.into()) }
+
+  &domain[domain.len() - bin.len()..]
+  // unsafe { String::from_utf8_unchecked(domain.into()) }
 }
 
 /// x.x.com:1131 -> x.com , 这个函数还会去掉端口号
-pub fn host_port_tld(host: impl AsRef<str>) -> String {
-  let host = host.as_ref();
-  host_tld(if let Some(p) = host.find(':') {
+pub fn host_port_tld(host: &str) -> &str {
+  tld(if let Some(p) = host.find(':') {
     &host[..p]
   } else {
     host
   })
 }
 
-pub fn url_host_port(url: impl AsRef<str>) -> String {
-  let url = url.as_ref();
+pub fn url_host_port(url: &str) -> &str {
   let begin = if let Some(pos) = url.find("://") {
     pos + 3
   } else {
@@ -49,6 +49,6 @@ pub fn url_host_port(url: impl AsRef<str>) -> String {
   url[..end].into()
 }
 
-pub fn url_tld(url: impl AsRef<str>) -> String {
+pub fn url_tld(url: &str) -> &str {
   host_port_tld(url_host_port(url))
 }
