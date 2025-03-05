f093453fff2f35e2f3b66e9b39d1c61eaea133aa
i18n <i18n.site@gmail.com>
2025-03-05 12:13:17 +0800

    set_cookie:0.1.4

--- set_cookie/src/lib.rs
+++ set_cookie/src/lib.rs
@@ -13,11 +13,23 @@ pub fn new(domain: impl Into<String>) -> Cookie {
 }
 
 impl Cookie {
+  pub fn set_max_for_js(&self, key: impl AsRef<str>, val: impl AsRef<str>) -> String {
+    self.set_for_js(key, val, MAX)
+  }
+
+  pub fn set_for_js(&self, key: impl AsRef<str>, val: impl AsRef<str>, max_age: u32) -> String {
+    let key = key.as_ref();
+    let val = val.as_ref();
+    format!(
+      "{key}={val};Max-Age={max_age};Domain={};Secure;Path=/;Partitioned",
+      self.domain
+    )
+  }
   pub fn set(&self, key: impl AsRef<str>, val: impl AsRef<str>, max_age: u32) -> String {
     let key = key.as_ref();
     let val = val.as_ref();
     format!(
-      "{key}={val};Max-Age={max_age};Domain={};Secure;HttpOnly;Path=/;Partitioned",
+      "{key}={val};Max-Age={max_age};Domain={};Secure;Path=/;Partitioned;HttpOnly",
       self.domain
     )
   }
