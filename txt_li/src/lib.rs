63fe11c253fa2e32065e09bc2b3a39ea7410797a
i18n <i18n.site@gmail.com>
2025-07-18 11:22:08 +0800

    txt_li:0.1.12

--- txt_li/src/lib.rs
+++ txt_li/src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(doc_auto_cfg)]
+#![feature(doc_cfg)]
+
 use anyhow::Result;
 
 pub mod restore;
@@ -29,6 +32,43 @@ impl TxtLi {
 
   pub fn push_no_tran_line(&mut self, txt: impl Into<String>) {
     self.push_no_tran(txt.into() + "\n");
+  }
+
+  #[cfg(feature = "push_trim_line")]
+  pub fn push_trim_line(&mut self, txt: impl Into<String>) {
+    let txt = txt.into();
+    let txt_len = txt.len();
+    let mut split_pos = txt_len;
+
+    let mut iter = txt.char_indices().peekable();
+    while let Some((pos, i)) = iter.next() {
+      if i == '-'
+        && let Some((_, next)) = iter.peek()
+        && (".-|:".contains(*next) || next.is_whitespace())
+      {
+        continue;
+      }
+
+      if "*_".contains(i)
+        && let Some((_, next)) = iter.peek()
+        && *next == i
+      {
+        split_pos = pos;
+        break;
+      }
+
+      if !("#>.:|=".contains(i) || i.is_whitespace()) {
+        split_pos = pos;
+        break;
+      }
+    }
+    if split_pos > 0 {
+      self.push_no_tran(&txt[..split_pos]);
+    }
+    if split_pos < txt_len {
+      self.push_tran(&txt[split_pos..]);
+    }
+    self.push_no_tran("\n");
   }
 }
 
