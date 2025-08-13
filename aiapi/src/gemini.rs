56bd993b537f1c06e58f452dab3414c9131e2fcc
i18n <i18n.site@gmail.com>
2025-08-13 17:00:30 +0800

    aiapi:0.2.6

--- aiapi/src/gemini.rs
+++ aiapi/src/gemini.rs
@@ -148,7 +148,6 @@ impl crate::AiApi for Gemini {
 
       if status.is_success() {
         text = response.text().await?;
-        // dbg!(&text);
         let chat_response: GeminiResponse = sonic_rs::from_str(&text)?;
         let usage_metadata = &chat_response.usage_metadata;
         if let Some(c) = chat_response.candidates.into_iter().next()
