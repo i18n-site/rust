baa92a58713c24a34ffa4f06cfef66e7e2b120bf
i18n <i18n.site@gmail.com>
2025-08-14 12:43:17 +0800

    preq:0.1.18

--- preq/README.md
+++ preq/README.md
@@ -9,7 +9,7 @@ async fn test() -> Result<()> {
   loginit::init();
   use preq::PROXY;
   // let url = IPV6_PROXY_TEST_URL.as_str();
-  let url = "https://www.baidu.com";
+  let url = "https://baidu.com/";
   let r = PROXY.post_form(url, [("q", "I")]).await?;
   tracing::info!("{}", String::from_utf8_lossy(&r));
   // assert_eq!(r, &*IPV6_PROXY_TEST_RESULT);
--- preq/src/lib.rs
+++ preq/src/lib.rs
@@ -4,13 +4,14 @@ use reqwest::IntoUrl;
 
 static mut N: usize = 0;
 
+pub struct Proxy;
+
 impl Proxy {
   pub async fn post_form(
-    &self,
     url: impl IntoUrl,
     form: impl IntoIterator<Item = (impl AsRef<str>, impl AsRef<str>)>,
   ) -> reqwest::Result<Bytes> {
     unsafe { (N, _) = N.overflowing_add(1) };
-    post_form(unsafe { N }, &self.0, url, form).await
+    post_form(unsafe { N }, &reqwest_client::PROXY, url, form).await
   }
 }
