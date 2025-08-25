3abfb4b3127b0ec0684f55ba42e5d6a01bf8938c
i18n <i18n.site@gmail.com>
2025-08-25 13:29:54 +0800

    .

--- i18n_gateway/src/error.rs
+++ i18n_gateway/src/error.rs
@@ -35,7 +35,7 @@ pub enum Error {
   S2nQuic(#[from] s2n_quic::provider::tls::s2n_tls::error::Error),
 
   #[error("s2n_quic provider: {0}")]
-  S2nQuicProvider(#[from] s2n_quic::provider::Error),
+  S2nQuicProvider(#[from] s2n_quic::provider::StartError),
 
   #[error("Invalid Host: {0}")]
   InvalidHost(#[from] http::uri::InvalidUri),
