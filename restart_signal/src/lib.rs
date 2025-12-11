ff801b7002f65a16449196c659d753ea9675a6bd
js0.site <jssite@googlegroups.com>
2025-12-11 18:15:57 +0800

    🔹 restart_signal:0.1.3

--- restart_signal/src/lib.rs
+++ restart_signal/src/lib.rs
@@ -2,13 +2,10 @@
 
 use signal_hook::{consts as signal_const, iterator::Signals};
 
-pub fn restart_signal() -> crossfire::MAsyncRx<()> {
-  let (shutdown_send, shutdown_recv) = crossfire::mpmc::bounded_tx_blocking_rx_async(1);
+pub fn restart_signal() -> crossfire::AsyncRx<()> {
+  let (shutdown_send, shutdown_recv) = crossfire::spsc::bounded_tx_blocking_rx_async(1);
   std::thread::spawn(move || {
     if let Ok(mut signals) = xerr::ok!(Signals::new([
-      // Hangup detected on controlling terminal or death of controlling process.
-      // 控制终端挂起或控制进程终止。
-      signal_const::SIGHUP,
       // Termination signal.
       // 终止信号。
       signal_const::SIGTERM,
@@ -18,6 +15,10 @@ pub fn restart_signal() -> crossfire::MAsyncRx<()> {
       // Quit from keyboard (Ctrl+\).
       // 来自键盘的退出 (Ctrl+\)。
       signal_const::SIGQUIT,
+      // Hangup detected on controlling terminal or death of controlling process.
+      // 控制终端挂起或控制进程终止。
+      #[cfg(feature = "sighup")]
+      signal_const::SIGHUP,
     ])) {
       signals.forever().next();
       let _ = shutdown_send.send(());
